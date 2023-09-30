94954abda25473a31ef686066e96200fa86dca56
siotan <nyaa.-525@outlook.jp>
2023-09-30 13:56:06 +0900

    ?

---
+++ three_practice/src/App.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+    <SBody>
+    </SBody>
+  );
+}
+
+const SBody = styled.div`
+  min-height: 100vh;
+  width: 100%;
+`;
+
+export default App;
